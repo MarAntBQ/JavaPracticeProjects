cd44275a25b9e3d46f0449153c5de55e9a3eb8b0
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-02 11:52:50 -0500

    Update Main.java

--- Project1/src/com/MarbustTechnologyCompany/Main.java
+++ Project1/src/com/MarbustTechnologyCompany/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("Hello world, this is my first code line in JAVA");
         System.out.println("Hello world, this is my first code line in JAVA");
+        System.out.print("Hello world, this is my first code line in JAVA");
     }
 }
