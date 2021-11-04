21c944600ced4322796e0229e688f8f47983e3c3
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-04 16:21:01 -0500

    Practicing more with Strings!

--- Project1/src/com/MarbustTechnologyCompany/Main.java
+++ Project1/src/com/MarbustTechnologyCompany/Main.java
@@ -4,7 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world, this is my first code line in JAVA");
-        System.out.print("Hello world, this is my first code line in JAVA");
+        int Number1 = 5;
+        int Number2 = 5;
+        int Suma = Number1 + Number2;
+        System.out.println("Enteros: " + Suma);
+        float Altura = 1.70f;
+        System.out.println("Variable flotante con decimales: " + Altura);
+        double valordePi = 3.141595415151515;
+        System.out.println("El valor de Pi es: " + valordePi);
+        long SunDistance = 4848888888885415151L;
+        System.out.println("El valor de Sol es: " + SunDistance);
+        boolean isDay = true;
+        System.out.println("El valor del día: " + isDay);
+        char miInicial = 'm';
+        System.out.println("Mi inicial es: " + miInicial);
+        String Marco = "Antonio Bustillos Quiroz";
+        System.out.println("Me llamo Marco " + Marco);
+        int tamano = Marco.length();
+        System.out.println("El tamaño de Marco es " + tamano);
+        boolean vacio= Marco.isEmpty();
+        System.out.println("La variable de Marco esta vacia: " + vacio);
+        boolean containString = Marco.contains("nt");
+        System.out.println("La variable de Marco contiene nt: " + containString);
+        String subString = Marco.substring(1, 5);
+        System.out.println("La variable de Marco contiene nt: " + subString);
+        String newApellido = Marco.replace("Bustillos", "Bustillos de Parra");
+        System.out.println("Me llamo Marco " + newApellido);
+        
+
     }
 }
