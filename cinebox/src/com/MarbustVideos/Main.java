e4fd9bc54c6027d187715a03c7cd8c10195b4645
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-04 18:16:46 -0500

    If / While!

--- cinebox/src/com/MarbustVideos/Main.java
+++ cinebox/src/com/MarbustVideos/Main.java
@@ -1,12 +1,22 @@
 package com.MarbustVideos;
 public class Main {
     public static void main(String[] args) throws Exception {
-        int totalEpisodes = 5;
+        int totalEpisodes = 1;
 
-        if (totalEpisodes == 1) {
-            System.out.println("It's a movie");
-        } else {
+        if (totalEpisodes > 1) {
             System.out.println("It's a show");
+        } else if (totalEpisodes == 0){
+            System.out.println("Movie or Show it's not available, please try again after");
+        } else {
+            System.out.println("It's a movie");
         }
+
+        //While
+        int finalEpisode = 10;
+        while(totalEpisodes <= finalEpisode) {
+            System.out.println("You're watching the Episode: " + totalEpisodes);
+            totalEpisodes++;
+        }
+
     }
 }
