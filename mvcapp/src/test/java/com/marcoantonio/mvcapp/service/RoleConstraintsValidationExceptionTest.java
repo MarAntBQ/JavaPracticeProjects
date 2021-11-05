5fc74dae1f9df583c409f398e55e063767896ce9
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-05 16:52:47 -0500

    Update RoleConstraintsValidationExceptionTest.java

--- mvcapp/src/test/java/com/marcoantonio/mvcapp/service/RoleConstraintsValidationExceptionTest.java
+++ mvcapp/src/test/java/com/marcoantonio/mvcapp/service/RoleConstraintsValidationExceptionTest.java
@@ -1,11 +1,36 @@
 package com.marcoantonio.mvcapp.service;
 
+import com.marcoantonio.mvcapp.dto.RoleDto;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class RoleConstraintsValidationExceptionTest {
+    @Autowired
+    private RoleServiceImpl roleService;
+
+    @Test
+    public void constraintsValidationExceptionTest() {
+        try {
+            roleService.create(new RoleDto(null));
+        } catch (Exception e) {
+            System.out.println("");
+        }
+        try {
+            roleService.create(new RoleDto(""));
+        } catch (Exception e) {
+            System.out.println("");
+        }
+        try {
+            roleService.create(new RoleDto("1234567890123456"));
+        } catch (Exception e) {
+            System.out.println("");
+        }
+    }
     
 }
