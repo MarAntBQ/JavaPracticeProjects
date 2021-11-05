d06e3effb2169d93a0b30151a321818a640706bb
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-04 20:30:50 -0500

    Creating a Model!

--- mvcapp/src/main/java/com/marcoantonio/mvcapp/MvcappApplication.java
+++ mvcapp/src/main/java/com/marcoantonio/mvcapp/MvcappApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.marcoantonio.mvcapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MvcappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MvcappApplication.class, args);
 	}
 
 }
--- mvcapp/src/test/java/com/marcoantonio/mvcapp/MvcappApplicationTests.java
+++ mvcapp/src/test/java/com/marcoantonio/mvcapp/MvcappApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.marcoantonio.mvcapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MvcappApplicationTests {
 
 	@Test
 	void contextLoads() {
