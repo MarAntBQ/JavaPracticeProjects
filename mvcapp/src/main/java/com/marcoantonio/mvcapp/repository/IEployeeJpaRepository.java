c18aeddde954ef0f6536e96c85a6369a1603d23c
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-05 13:12:12 -0500

    Done with the Creation of DataBases!

--- mvcapp/src/main/java/com/marcoantonio/mvcapp/repository/IEployeeJpaRepository.java
+++ mvcapp/src/main/java/com/marcoantonio/mvcapp/repository/IEployeeJpaRepository.java
@@ -1,6 +1,9 @@
 package com.marcoantonio.mvcapp.repository;
 
+import java.util.List;
+
 import com.marcoantonio.mvcapp.model.Employee;
+import com.marcoantonio.mvcapp.model.Role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +12,10 @@ import org.springframework.stereotype.Repository;
 public interface IEployeeJpaRepository extends JpaRepository<Employee, Long> {
     //Select fields from eployee where eployeeid=['param']
     Employee findByEmployeeid(String employeeid);
+
+    List<Employee> findByFirstName(String firstName);
+
+    List<Employee> findByLastName(String lastName);
+
+    List<Employee> findByRole(Role role);
 }
--- mvcapp/src/test/java/com/marcoantonio/mvcapp/repository/ProjectJpaTest.java
+++ mvcapp/src/test/java/com/marcoantonio/mvcapp/repository/ProjectJpaTest.java
@@ -3,7 +3,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.marcoantonio.mvcapp.model.Project;
-import com.marcoantonio.mvcapp.repository.IProjectJpaRepository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
