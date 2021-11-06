37c76b991d34d4018323231b2a50324f9ef3c84f
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-05 23:08:26 -0500

    For today it's enogh, it's time to Rest!

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,9 @@ public class StudentController {
 
     private final StudentService studentService;
 
+
+    //https://youtu.be/9SGDpanrc8U?t=1969
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
