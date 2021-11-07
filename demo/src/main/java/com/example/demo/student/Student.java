15c765eaff412794299f6f2383ad0c8cf6a37c95
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-06 19:43:18 -0500

    Settings!

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -2,7 +2,20 @@ package com.example.demo.student;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Student {
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.AUTO,
+        generator = "student_sequence"
+        )
     private Long id;
     private String name;
     private String email;
--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -4,13 +4,23 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-    public List<Student> getStudents() {
-		return List.of(
-			new Student(1L, "Ana Veronica", "soporte@anaveronicaparra.com", LocalDate.of(1991, Month.JUNE, 12), 21)
-		);
+
+	private final StudentRepository studentRepository;
+
+	
+	@Autowired
+    public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
+
+
+	public List<Student> getStudents() {
+		return studentRepository.findAll();
 	}
 }
