1d83bfe223d8eb0dc8e94173e44b1beb5093f99a
MarAntBQ <github@marantbq.dev>
2025-01-03 21:54:20 -0500

    Agregar clase MainProgram con mensaje de saludo personalizado

---
+++ 02-Course/MainProgram.java
@@ -0,0 +1,7 @@
+public class MainProgram {
+  public static void main(String[] args) {
+    // Declare variable string to store "MarAntBQ.dev"
+    String name = "MarAntBQ.dev";
+    System.out.println("Hello World from: " + name);
+  }
+}
