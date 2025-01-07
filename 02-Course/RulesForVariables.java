dcedf7f1bc9939f12db8cc3541c63757634df0b1
MarAntBQ <github@marantbq.dev>
2025-01-07 00:12:20 -0500

    Agregar clase RulesForVariables con ejemplos de uso de variables

---
+++ 02-Course/RulesForVariables.java
@@ -0,0 +1,10 @@
+public class RulesForVariables {
+  public static void main(String[] args) {
+    int _myAge = 28;
+    System.out.println("My age is: " + _myAge);
+    int $myAge = 28;
+    System.out.println("My age is: " + $myAge);
+    
+  }
+
+}
